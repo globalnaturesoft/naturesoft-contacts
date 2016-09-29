9c9735ab84f053b31cfb1bfbaa6006e7dc48cad3
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-29 09:42:11 -0700

    update nsmodule (contact_info) for contacts engine

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,11 @@
+Naturesoft::Nsmodule.class_eval do
+  @contacts = {
+    "contact_info" => {
+      "label" => "Contact Info Box (Site Info)",
+      "options" => {
+        "id" => nil,
+        "style" => "default"
+      }
+    }
+  }
+end
