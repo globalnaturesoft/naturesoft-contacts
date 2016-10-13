5c11572d9e557f5342b443c304d605d741c71bd8
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-10-13 00:28:18 -0700

    update format message

--- app/models/naturesoft/contacts/message.rb
+++ app/models/naturesoft/contacts/message.rb
@@ -2,5 +2,10 @@ module Naturesoft::Contacts
   class Message < ApplicationRecord
     belongs_to :contact, class_name: "Naturesoft::Contacts::Contact"
     belongs_to :to_contact, class_name: "Naturesoft::Contacts::Contact", foreign_key: :to_contact_id
+    
+    def display_message
+      html = message.gsub("\r\n", "<br/>")
+      return html.html_safe
+    end
   end
 end
