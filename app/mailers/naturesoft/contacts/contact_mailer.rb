fa59fa11a614cac106cfddc58859e37701e282fd
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-11-10 23:42:32 -0800

    translate subject contact send mail

--- app/mailers/naturesoft/contacts/contact_mailer.rb
+++ app/mailers/naturesoft/contacts/contact_mailer.rb
@@ -2,7 +2,7 @@ module Naturesoft::Contacts
   class ContactMailer < Naturesoft::ApplicationMailer
     def sending_email_contact(msg)
       @msg = msg
-      send_email(@msg.to_contact.email, "[Notifications] You have received a message from #{@msg.contact.email}")
+      send_email(@msg.to_contact.email, "#{t(:subject_send_email_contact)} #{@msg.contact.email}")
     end
   end
 end
